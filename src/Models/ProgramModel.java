4da5f774fda0773437487cb8a4a10acac99bf001
Joseph Johnston <joe.a.johnston@gmail.com>
2013-12-21 16:27:59 -0500

    Added ProjectModel parent class. and started on test classes. first commit using netbeans

---
+++ src/Models/ProgramModel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Arthur
+ */
+public class ProgramModel {
+    private ProjectModel project;
+    private ArrayList classes;
+    
+    
+    
+}
---
+++ src/Models/VariableModel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Arthur
+ */
+public class VariableModel {
+    private String name;
+    private Object value;
+    
+    public VariableModel(String name, Object value){
+        this.value = value;
+        this.name = name;
+    }
+    
+    
+}
